3354b6f86c86ade2d5319ead6796818e9a5130a8
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-08 10:43:23 +0100

    Add files via upload

---
+++ Part 2/1 Classes Demos/TextBox.java
@@ -0,0 +1,12 @@
+package com.codewithmosh;
+
+public class TextBox {
+  public String text = "";
+
+  public void setText(String text) {
+    this.text = text;
+  }
+  public void clear() {
+    text = "";
+  }
+}
