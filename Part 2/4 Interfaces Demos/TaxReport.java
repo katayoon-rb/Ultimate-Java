ea1a5527ee4031018bdbb01139cc9004804ed028
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-08 13:20:31 +0330

    Update TaxReport.java

--- Part 2/4 Interfaces Demos/TaxReport.java
+++ Part 2/4 Interfaces Demos/TaxReport.java
@@ -12,8 +12,4 @@ public class TaxReport {
     var tax = calculator.calculateTax();
     System.out.println(tax);
   }
-
-//  public void setCalculator(TaxCalculator calculator) {
-//    this.calculator = calculator;
-//  }
 }
